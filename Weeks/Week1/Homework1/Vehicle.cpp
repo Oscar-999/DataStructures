ea1b0695196745232ed37faa6f45d2e58fdd84ab
Quai <quai@Quais-MacBook-Air.local>
2025-05-23 21:07:30 -0500

    week1

---
+++ Weeks/Week1/Homework1/Vehicle.cpp
@@ -0,0 +1,8 @@
+//Oscar Alcantar
+//Week1 Homework
+//Description: Write a class and driver to implement functionality related to vehicles. Display a menu of
+//options to the user, and implement the functionality to add, delete, and search for a
+//vehicle, and to display all vehicles.
+
+
+#include "Vehicle.h"
---
+++ Weeks/Week1/Homework1/Vehicle.h
@@ -0,0 +1,16 @@
+//Oscar Alcantar
+//Week1 Homework
+//Description: Write a class and driver to implement functionality related to vehicles. Display a menu of
+//options to the user, and implement the functionality to add, delete, and search for a
+//vehicle, and to display all vehicles.
+
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+class Vehicle {
+
+};
+
+
+
+#endif //VEHICLE_H
