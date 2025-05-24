0666e760b010a5dd04cf977759e18f20931c0aae
Quai <quai@Quais-MacBook-Air.local>
2025-05-24 11:07:49 -0500

    cpp

--- Weeks/Week1/Homework1/Vehicle.cpp
+++ Weeks/Week1/Homework1/Vehicle.cpp
@@ -6,3 +6,52 @@
 
 
 #include "Vehicle.h"
+#include <iomanip>
+#include <sstream>
+using namespace std;
+#include <string>
+//Default con
+
+Vehicle::Vehicle() {vin = 0;}
+
+//3 param
+Vehicle::Vehicle(string m, string mod, int vi) {
+  make = m;
+  model = mod;
+  vin = vi;
+}
+
+//setters
+void Vehicle::setMake(string m) {
+  make = m;
+}
+void Vehicle::setModel(string m) {
+  model = m;
+}
+void Vehicle::setVin(int v) {
+  vin = v;
+}
+
+//getter
+
+string Vehicle::getMake() const{
+  return make;
+}
+
+string Vehicle::getModel() const{
+  return model;
+}
+
+int Vehicle::getVin() const{
+  return vin;
+}
+
+//Format output
+string Vehicle::toString() const {
+  stringstream ss;
+  ss << left << setw(10) << make
+        << setw(10) << model
+        << setw(5) << setfill('0') << vin;
+  return ss.str();
+}
+
--- Weeks/Week1/Homework1/Vehicle.h
+++ Weeks/Week1/Homework1/Vehicle.h
@@ -6,9 +6,33 @@
 
 #ifndef VEHICLE_H
 #define VEHICLE_H
+#include <string>
+using namespace std;
+
 
 class Vehicle {
+//Data memebers
 
+private:
+  string make;
+  string model;
+  int vin;
+public:
+  Vehicle();
+  Vehicle(string make, string model, int vin);
+
+  //setters
+  void setMake(string make);
+  void setModel(string model);
+  void setVin(int vin);
+
+  //getters
+  string getMake() const;
+  string getModel() const;
+  int getVin() const;
+
+  //Displays
+  string toString() const;
 };
 
 
